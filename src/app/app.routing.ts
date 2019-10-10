0f0c3a43ab7509362b1a3332ae566dbacb399834
David <davidtn52@gmail.com>
2019-10-10 08:18:13 +0200

    iniciando ..

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -3,12 +3,14 @@ import { CommonModule, } from '@angular/common';
 import { BrowserModule  } from '@angular/platform-browser';
 import { Routes, RouterModule } from '@angular/router';
 import {CatsComponent} from '../app/cats/cats.component';
+// import {AppmapComponent} from '../app/component/appmap/appmap.component';
 
 
 // import { AdminLayoutComponent } from './layouts/admin-layout/admin-layout.component';
 
 const routes: Routes =[
     {path: '', component: CatsComponent},
+    // {path : 'mapa', component: AppmapComponent }
     
 ];
 
