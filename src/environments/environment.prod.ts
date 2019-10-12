c65b73bcc64b3e87c8d565ebe6bc82cbf98bdc24
David <davidtn52@gmail.com>
2019-10-12 21:44:43 +0200

    error de nuevo

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,15 @@
 export const environment = {
-  production: true
+  production: true,
+  mapbox: {
+    accessToken: "pk.eyJ1IjoiZGVpdmkwMDciLCJhIjoiY2sxa3JnbzI0MDBtczNvcGZ3bTluZjI1ZyJ9.RjokMYjenPdPzqQ0-4Mctg"
+  },
+  firebase : {
+    apiKey: "AIzaSyBNku5N-xkSPxpYdvj200h7sFH205e-sRA",
+    authDomain: "gtemergency-4b876.firebaseapp.com",
+    databaseURL: "https://gtemergency-4b876.firebaseio.com",
+    projectId: "gtemergency-4b876",
+    storageBucket: "gtemergency-4b876.appspot.com",
+    messagingSenderId: "956042890669",
+    appId: "1:956042890669:web:a46b4f006b87594b98e524"
+  }
 };
